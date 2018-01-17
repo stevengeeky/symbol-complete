0bc6c1d9b4ce8ee87b7bcbfb9e679bbc3715cfae
stevengeeky <stevengeeky@gmail.com>
2018-01-17 00:38:08 -0500

    minor undo issue fixed

--- extension.js
+++ extension.js
@@ -5,6 +5,8 @@
  */
 const vscode = require('vscode');
 
+let just_replaced = false;
+
 function activate(context) {
     var config = vscode.workspace.getConfiguration("symbol-complete");
     var replacements = config.get("replacements");
@@ -24,6 +26,8 @@ function update(replacements) {
     var line = document.lineAt(selection.start).text;
     var line_to_cursor = line.substring(0, selection.start.character);
     
+    if (just_replaced) return (just_replaced = false);
+    
     if (selection.isEmpty) {
         for (var k in replacements) {
             var pattern = k;
@@ -32,8 +36,10 @@ function update(replacements) {
             if (reg.test(line_to_cursor)) {
                 var value = line_to_cursor.substring(line_to_cursor.match(reg).index);
                 if (value.length > 0) {
+                    editor.selection = new vscode.Selection(selection.start.line, selection.start.character - value.length, selection.start.line, selection.start.character);
                     editor.edit(function(editBuilder) {
-                        editBuilder.replace(new vscode.Selection(selection.start.line, selection.start.character - value.length, selection.start.line, selection.start.character), replacements[k]);
+                        editBuilder.replace(editor.selection, replacements[k]);
+                        just_replaced = true;
                     }).then(function() {
                         if (!editor.selection.isEmpty) {
                             editor.selection = new vscode.Selection(selection.start.line, selection.start.character, selection.start.line, selection.start.character);
